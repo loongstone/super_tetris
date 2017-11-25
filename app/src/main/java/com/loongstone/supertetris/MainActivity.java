53ebc4f69dd806328288d5ffbe522d90822c31d5
loongstone <loongstone@126.com>
2017-11-25 22:22:09 +0800

    init

---
+++ app/src/main/java/com/loongstone/supertetris/MainActivity.java
@@ -0,0 +1,16 @@
+package com.loongstone.supertetris;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author loongstone
+ * @date 2017/11/25 21:59
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
